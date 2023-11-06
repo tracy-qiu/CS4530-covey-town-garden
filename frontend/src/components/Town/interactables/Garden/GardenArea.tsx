eb774693ed8e7137d2ad5f9a7059cd07786be53e
linnielin <madisonlin5370@gmail.com>
2023-11-05 23:17:18 -0500

    modal with some grid

--- frontend/src/components/Town/interactables/Garden/GardenArea.tsx
+++ frontend/src/components/Town/interactables/Garden/GardenArea.tsx
@@ -4,6 +4,7 @@ import { useInteractable, useInteractableAreaController } from '../../../../clas
 import useTownController from '../../../../hooks/useTownController';
 import { InteractableID } from '../../../../types/CoveyTownSocket';
 import GameAreaInteractable from '../GameArea';
+import { extendTheme } from '@chakra-ui/react';
 import {
   Modal,
   ModalOverlay,
@@ -14,15 +15,59 @@ import {
   Spacer,
   Grid,
   GridItem,
+  chakra,
+  Button,
 } from '@chakra-ui/react';
 
-function GardenArea({ interactableID }: { interactableID: InteractableID }): JSX.Element {
+const customTheme = extendTheme({
+  colors: {
+    custom: {
+      primary: '793D00',
+    },
+  },
+});
+
+const StyledPlot = chakra(Button, {
+  baseStyle: {
+    borderRadius: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexBasis: '33%',
+    border: '1px solid teal',
+    color: '793D00',
+    height: '33%',
+    fontSize: '50px',
+    _disabled: {
+      opacity: '100%',
+    },
+  },
+});
+
+const StyledGarden = chakra(Container, {
+  baseStyle: {
+    display: 'flex',
+    width: '400px',
+    height: '400px',
+    padding: '5px',
+    flexWrap: 'wrap',
+  },
+});
+
+export function GardenArea({ interactableID }: { interactableID: InteractableID }): JSX.Element {
+  const garden: number[][] = [
+    [1, 1, 0, 0],
+    [1, 1, 0, 0],
+  ];
   return (
     <Container>
       <Spacer />
-      <Grid>
-        <GridItem></GridItem>
-      </Grid>
+      <StyledGarden>
+        {garden.map((row, rowIndex) => {
+          return row.map((plot, colIndex) => {
+            return <StyledPlot key={`${rowIndex}.${colIndex}`}></StyledPlot>;
+          });
+        })}
+      </StyledGarden>
     </Container>
   );
 }
@@ -45,10 +90,10 @@ export default function TicTacToeAreaWrapper(): JSX.Element {
 
   if (gameArea && gameArea.getData('type') === 'TicTacToe') {
     return (
-      <Modal isOpen={true} onClose={closeModal} closeOnOverlayClick={false}>
+      <Modal isOpen={true} onClose={closeModal} closeOnOverlayClick={false} size='xl'>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>{gameArea.name}</ModalHeader>
+          <ModalHeader>{'Community Garden'}</ModalHeader>
           <ModalCloseButton />
           <GardenArea interactableID={gameArea.name} />;
         </ModalContent>
