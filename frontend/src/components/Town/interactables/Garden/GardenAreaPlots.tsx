6cce84e41a625c7e4481afbdd1b433baba195bb2
linnielin <madisonlin5370@gmail.com>
2023-11-13 14:52:01 -0500

    name changes + start plot button refactor

--- frontend/src/components/Town/interactables/Garden/GardenAreaPlots.tsx
+++ frontend/src/components/Town/interactables/Garden/GardenAreaPlots.tsx
@@ -1,57 +1,55 @@
-import React from 'react';
-import { Container, Box, chakra, SimpleGrid } from '@chakra-ui/react';
-import { GardenPlotButton } from './PlotButton';
-import { Plant, PlotPlant } from '../../../../types/CoveyTownSocket';
+import React, { useCallback, useEffect, useState } from 'react';
+//import PlayerController from '../../../../classes/PlayerController';
+import { useInteractable, useInteractableAreaController } from '../../../../classes/TownController';
+import useTownController from '../../../../hooks/useTownController';
+import { InteractableID, Plant } from '../../../../types/CoveyTownSocket';
+import GameAreaInteractable from '../GameArea';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  Container,
+  Spacer,
+  Grid,
+  GridItem,
+  Box,
+  chakra,
+  Button,
+  HStack,
+  VStack,
+} from '@chakra-ui/react';
+import PlantCare from './Plant/PlantCare';
 
-const samplePlant1: PlotPlant = {
-  plantId: '12',
-  plant: {
-    pid: '1',
-    name: 'blueberry',
-    species: 'Blueberry',
-    age: 'Seedling',
-    lastWatered: new Date(),
-    status: 'Healthy',
+const StyledPlot = chakra(Button, {
+  baseStyle: {
+    borderRadius: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexBasis: '25%',
+    borderColor: '#EDD4B2',
+    borderWidth: '2px',
+    bgColor: '#793D00',
+    height: '25%',
+    width: '25%',
+    fontSize: '50px',
+    _disabled: {
+      opacity: '100%',
+    },
+    _hover: { backgroundColor: '#C4A484' },
   },
-};
+});
 
-const samplePlant2: PlotPlant = {
-  plantId: '13',
-  plant: {
-    pid: '2',
-    name: 'Rosalina',
-    species: 'Rose',
-    age: 'Sprout',
-    lastWatered: new Date(),
-    status: 'Dehydrated',
+const StyledGarden = chakra(Container, {
+  baseStyle: {
+    display: 'flex',
+    width: '400px',
+    height: '400px',
+    padding: '0px',
+    flexWrap: 'wrap',
   },
-};
-
-const samplePlant3: PlotPlant = {
-  plantId: '14',
-  plant: {
-    pid: '3',
-    name: 'Bugs',
-    species: 'Carrot',
-    age: 'Adult',
-    lastWatered: new Date(),
-    status: 'About to Die',
-  },
-};
-
-const undefinedPlant: PlotPlant = { plantId: '16', plant: undefined };
-
-const PLANTS: PlotPlant[] = [samplePlant1, samplePlant2, samplePlant3, undefinedPlant];
-
-// const StyledGarden = chakra(Container, {
-//   baseStyle: {
-//     display: 'flex',
-//     width: '75%',
-//     height: '400px',
-//     padding: '0px',
-//     flexWrap: 'wrap',
-//   },
-// });
+});
 
 /**
  * Renders each individual plot styled for the garden together as one component.
@@ -60,37 +58,41 @@ const PLANTS: PlotPlant[] = [samplePlant1, samplePlant2, samplePlant3, undefined
  * @returns {JSX.Element} GardenPlots
  */
 export function GardenAreaPlots(): JSX.Element {
-  const users: string[] = [
-    'user1',
-    'user2',
-    'user3',
-    'me',
-    'long username',
-    'katherine',
-    'tracy',
-    'surabhi',
-    'madison',
-    'user5',
-    'user6',
-    'user7',
-    'user8',
+  const garden: number[][] = [
+    [1, 1, 0, 0],
+    [1, 1, 0, 1],
   ];
 
+  const [viewPlot, setViewPlot] = useState(false);
+
+  //useState -> gardenPlots
+
+  //useState -> pop up another modal on top of first? replace modal?
+
+  //useEffect -> tracks garden controller
+
+  // const samplePlant: Plant = {
+  //   pid: 'ajsfd',
+  //   name: 'blueberry',
+  //   species: 'Blueberry',
+  //   age: 'Seedling',
+  //   lastWatered: new Date(),
+  //   status: 'Healthy',
+  // };
   return (
-    <Container>
-      <SimpleGrid spacing={3} columns={4}>
-        {users.map((username, index) => {
-          return (
-            <Box key={username}>
-              <GardenPlotButton
-                fontSize='20px'
-                key={`${index}`}
-                username={username}
-                plants={PLANTS}></GardenPlotButton>
-            </Box>
-          );
+    <Box overflowX='auto'>
+      <StyledGarden>
+        {garden.map((row, rowIndex) => {
+          return row.map((plot, colIndex) => {
+            return (
+              <StyledPlot key={`${rowIndex}.${colIndex}`} onClick={console.log('clicked a plot!')}>
+                {plot}
+              </StyledPlot>
+              // <PlantCare plant={samplePlant} showActions={true} key={`${rowIndex}.${colIndex}`} />
+            );
+          });
         })}
-      </SimpleGrid>
-    </Container>
+      </StyledGarden>
+    </Box>
   );
 }
