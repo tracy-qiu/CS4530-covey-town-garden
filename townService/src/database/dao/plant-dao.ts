483b8efc21fcd82159b7befc20c5d37a9bbe064f
Tracy Qiu <qiu.tr@northeastern.edu>
2023-11-22 10:06:59 -0500

    add and delete plants

--- townService/src/database/dao/plant-dao.ts
+++ townService/src/database/dao/plant-dao.ts
@@ -1,8 +1,9 @@
+import mongoose from 'mongoose';
 import { PlantDB, plantModel } from '../schema';
 
 export const findPlants = () => plantModel.find();
 export const findPlantById = (id: string) => plantModel.findById(id);
 export const createPlant = (plant: PlantDB) => plantModel.create(plant);
-export const deletePlant = (pid: number) => plantModel.deleteOne({ _id: pid });
+export const deletePlant = (pid: mongoose.Types.ObjectId) => plantModel.deleteOne({ _id: pid });
 export const updatePlant = (pid: number, plant: PlantDB) =>
   plantModel.updateOne({ _id: pid }, { $set: plant });
