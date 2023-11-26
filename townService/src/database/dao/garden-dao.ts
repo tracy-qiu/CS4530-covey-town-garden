637aa529d4950049e0a14d2f2378492412dd3468
Tracy Qiu <qiu.tr@northeastern.edu>
2023-11-26 15:48:43 -0500

    all endpoints except update complete

--- townService/src/database/dao/garden-dao.ts
+++ townService/src/database/dao/garden-dao.ts
@@ -1,8 +1,11 @@
+import mongoose from 'mongoose';
 import { GardenDB, gardenModel } from '../schema';
 
 export const findGardens = () => gardenModel.find();
-export const findGardenById = (id: string) => gardenModel.findById(id);
+export const findGardenById = (gid: mongoose.Types.ObjectId) => gardenModel.findById(gid);
+export const findGardenByTownId = (tid: mongoose.Types.ObjectId) =>
+  gardenModel.find({ townId: tid });
 export const createGarden = (garden: GardenDB) => gardenModel.create(garden);
-export const deleteGarden = (gid: number) => gardenModel.deleteOne({ _id: gid });
-export const updateGarden = (gid: number, garden: GardenDB) =>
-  gardenModel.updateOne({ _id: gid }, { $set: garden });
+export const deleteGarden = (gid: mongoose.Types.ObjectId) => gardenModel.deleteOne({ _id: gid });
+export const updateGarden = (gid: mongoose.Types.ObjectId, plotId: string) =>
+  gardenModel.updateOne({ _id: gid }, { $push: { gardenPlots: plotId } });
--- townService/src/database/dao/gardener-dao.ts
+++ townService/src/database/dao/gardener-dao.ts
@@ -1,7 +1,11 @@
+import mongoose from 'mongoose';
 import { GardenerDB, gardenerModel } from '../schema';
 
-export const findGardener = () => gardenerModel.find();
-export const createGardener = (garden: GardenerDB) => gardenerModel.create(garden);
-export const deleteGardener = (gid: number) => gardenerModel.deleteOne({ _id: gid });
-export const updateGardener = (gid: number, garden: GardenerDB) =>
+export const findGardeners = (gardenId: mongoose.Types.ObjectId) =>
+  gardenerModel.find({ gardenId });
+export const findGardenerById = (id: mongoose.Types.ObjectId) => gardenerModel.findById(id);
+export const createGardener = (gardener: GardenerDB) => gardenerModel.create(gardener);
+export const deleteGardener = (gid: mongoose.Types.ObjectId) =>
+  gardenerModel.deleteOne({ _id: gid });
+export const updateGardener = (gid: mongoose.Types.ObjectId, garden: GardenerDB) =>
   gardenerModel.updateOne({ _id: gid }, { $set: garden });
