836c7d96c302d8b8237c69ac6ce11483215f4b8e
Tracy Qiu <qiu.tr@northeastern.edu>
2023-11-28 22:04:05 -0500

    revert town controller, test and server

--- townService/src/town/garden/GardenController.ts
+++ townService/src/town/garden/GardenController.ts
@@ -345,7 +345,7 @@ export class GardenController extends Controller {
    * @param gardenPlotId
    * @returns response of deleteGardenPlot
    */
-  @Delete('/plots/{gardenId}')
+  @Delete('/plots/{gardenPlotId}')
   public async deletePlot(
     @Path()
     gardenPlotId: string,
